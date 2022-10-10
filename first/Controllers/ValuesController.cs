eb07c94504ee77a3ca674f417d11bf336d95d772
khalin1337 <84382013+khalin1337@users.noreply.github.com>
2022-10-10 05:41:45 +0300

    Добавьте файлы проекта.

---
+++ first/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace first.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+        // POST api/<ValuesController>
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            try
+            {
+                string sum = new DataTable().Compute(value, null).ToString();
+                return sum;
+            }
+            catch (Exception) {
+
+                return "Не правильный формат";
+            }
+        }
+
+    }
+}
